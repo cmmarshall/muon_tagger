3c3832b40a78795c6d5fb4ca6c450323caf60d28
Christopher Marshall <marshall@pas.rochester.edu>
2021-02-25 08:20:34 -0600

    split out the multiplicity by where the rock muons enter

--- rock_muons.py
+++ rock_muons.py
@@ -10,10 +10,18 @@ rock_pot = 8.89006E15 # this is only ~100 spills, not great
 # hard-coded position of the LAr active volume relative to (0,0,0) at the center of the upstream face
 offset = [ 0., 5., 410. ]
 
+# where did the muon enter the detector from
+entries = ["front", "side", "top", "bottom"]
+colors = [ROOT.kGreen+2, ROOT.kRed, ROOT.kBlue, ROOT.kCyan+1]
+
 class Muon:
     def __init__(self, entry=None, exit=None):
-        self.entry = entry
-        self.exit = exit
+        self.entry = ROOT.TVector3(entry.x()/10. - offset[0], entry.y()/10. - offset[1], entry.z()/10. - offset[2])
+        self.exit =  ROOT.TVector3(exit.x()/10. - offset[0], exit.y()/10. - offset[1], exit.z()/10. - offset[2])
+        # Determine whether the muon entered from the front, sides, top/bottom
+        dist_to_edge = [abs(self.entry.z()-0.), min(abs(self.entry.x()+357.), abs(self.entry.x()-357.)), abs(self.entry.y()-150.), abs(self.entry.y()+150.)]
+        smallest = min(dist_to_edge)
+        self.where = entries[dist_to_edge.index(smallest)]
 
     def setEntry(self, v):
         self.entry = ROOT.TVector3(v.x()/10. - offset[0], v.y()/10. - offset[1], v.z()/10. - offset[2])
@@ -32,6 +40,7 @@ def DoEvent( event ):
 
     # Loop over ArgonCube energy deposits
     muon = None
+    nonMuonEnergy = 0.
     for hit in hits: # use the hits to find muons that go into the detector
         hitpdg = event.Trajectories[hit.Contrib[0]].PDGCode # pdg code of the thing that made the hit
         if abs(hitpdg) == 13: # hit due to a muon
@@ -79,12 +88,19 @@ def loop( RockEvents, h, spill_pot ):
 
         # Fill histograms
         h["mult"].Fill( len(muons) )
+        n = {}
+        for entry in entries: n[entry] = 0
         for muon in muons:
             h["entry_xy"].Fill( muon.entry.x(), muon.entry.y() )
-            if muon.entry.z() > 10. and muon.entry.y() > 140.:
+            if muon.where == "top":
                 h["entry_ztop"].Fill( muon.entry.z() )
-            elif muon.entry.z() > 10. and abs(muon.entry.y()) < 140.:
+            elif muon.where == "side":
                 h["entry_zside"].Fill( muon.entry.z() )
+
+            n[muon.where] += 1
+
+        for entry in entries:
+            h["mult_%s" % entry].Fill( n[entry] )
 
         ispill += 1
 
@@ -121,15 +137,31 @@ if __name__ == "__main__":
 
     # output histograms
     h = {}
-    h["mult"] = ROOT.TH1D( "muon_multiplicity", ";Number of muons", 50, 0., 50. )
-    h["entry_xy"] = ROOT.TH2D( "entry_xy", ";Entry x (cm);Entry y (cm)", 180, -360., 360., 80, -160., 160. )
-    h["entry_ztop"] = ROOT.TH1D( "entry_ztop", ";Entry z (cm)", 265, -10., 520. )
-    h["entry_zside"] = ROOT.TH1D( "entry_zside", ";Entry z (cm)", 265, -10., 520. )
+    h["mult"] = ROOT.TH1D( "muon_multiplicity", ";Number of muons;Fraction of 1.2MW spills", 30, 0., 30. )
+    for entry in entries:
+        h["mult_%s" % entry] = ROOT.TH1D( "mult_%s" % entry, ";Muons per spill", 30, 0., 30. )
+    h["entry_xy"] = ROOT.TH2D( "entry_xy", ";Entry x (cm);Entry y (cm)", 72, -360., 360., 32, -160., 160. )
+    h["entry_ztop"] = ROOT.TH1D( "entry_ztop", ";Entry z (cm)", 25, 0., 500. )
+    h["entry_zside"] = ROOT.TH1D( "entry_zside", ";Entry z (cm)", 25, 0., 500. )
 
     loop( RockEvents, h, 7.5E13 )
 
     fout = ROOT.TFile( args.outfile, "RECREATE" )
     for key in h:
         h[key].Write()
+
+    c = ROOT.TCanvas()
+    leg = ROOT.TLegend(0.6, 0.5, 0.846, 0.846)
+    leg.AddEntry( h["mult"], "All", "l" )
+    h["mult"].Scale( 1. / h["mult"].Integral() )
+    h["mult"].SetMaximum(0.4)
+    h["mult"].Draw("hist")
+    for i,entry in enumerate(entries):
+        h["mult_%s" % entry].Scale( 1. / h["mult_%s" % entry].Integral() )
+        h["mult_%s" % entry].SetLineColor(colors[i])
+        leg.AddEntry( h["mult_%s" % entry], entry.capitalize(), "l" )
+        h["mult_%s" % entry].Draw("hist same")
+    leg.Draw()
+    c.Print( "muon_multiplicity.png" )
 
 
